73f7bb00acc9e1057e2c49f728d6b2efba1af1ac
Evgeny Mosunov <eugene.mosunov@saritasa.com>
2018-06-08 14:56:23 +0500

    Fix Chat message model updated_at constant.

--- src/Models/ChatMessage.php
+++ src/Models/ChatMessage.php
@@ -28,7 +28,7 @@ class ChatMessage extends Model implements IChatMessage
     public const USER_ID = 'user_id';
     public const MESSAGE = 'message';
     public const CREATED_AT = 'created_at';
-    public const UPDATED_AT = 'update_at';
+    public const UPDATED_AT = 'updated_at';
 
     protected $fillable = [
         self::CHAT_ID,
